dc49f60209b02251790589836f3293584505780c
Anirudha Pradhan <anirudhapradhan403@gmail.com>
2024-01-15 01:04:59 +0530

    init

---
+++ src/app/nav/nav.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+// import {MatSlideToggleModule} from '@angular/material/slide-toggle'
+@Component({
+  selector: 'app-nav',
+  standalone: true,
+  imports: [],
+  templateUrl: './nav.component.html',
+  styleUrl: './nav.component.scss'
+})
+export class NavComponent {
+
+}
